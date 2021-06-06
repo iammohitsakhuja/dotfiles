33aaaa030484100197bb86a2e197ed97bb5d8f45
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2021-06-06 22:23:47 +0530

    feat: update macos packages

--- macos/scripts/packages.sh
+++ macos/scripts/packages.sh
@@ -37,21 +37,26 @@ PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
 # Install mas-cli for Mac App Store applications.
 brew install mas
 
-mas install 1365531024 # 1Blocker for Safari.
-mas install 937984704  # Amphetamine.
-mas install 595191960  # CopyClip.
-# mas install 1475897096 # JIRA Cloud by Atlassian.
-mas install 409183694  # Keynote.
-mas install 926036361  # LastPass Password Manager.
-mas install 409201541  # Pages.
-mas install 1160374471 # Pipifier.
-mas install 823766827  # OneDrive.
-mas install 803453959  # Slack.
-mas install 1153157709 # Speedtest.
-mas install 425424353  # The Unarchiver.
-# mas install 966085870  # TickTick.
-mas install 1284863847 # Unsplash Wallpapers.
-mas install 1147396723 # WhatsApp Desktop.
+mas install 1365531024      # 1Blocker.
+mas install 937984704       # Amphetamine.
+mas install 1352778147      # Bitwarden.
+mas install 595191960       # CopyClip.
+# mas install 411643860       # DaisyDisk.
+# mas install 1475897096      # Jira.
+mas install 409183694       # Keynote.
+# mas install 1480068668      # Messenger.
+mas install 409203825       # Numbers.
+mas install 823766827       # OneDrive.
+mas install 409201541       # Pages.
+mas install 1160374471      # PiPifier.
+mas install 803453959       # Slack.
+mas install 1153157709      # Speedtest.
+# mas install 747648890       # Telegram.
+mas install 425424353       # The Unarchiver.
+# mas install 966085870       # TickTick.
+# mas install 1482454543      # Twitter.
+mas install 1284863847      # Unsplash Wallpapers.
+mas install 1147396723      # WhatsApp Desktop.
 
 # Remove outdated versions from the cellar.
 echo "Doing some cleanup..."
