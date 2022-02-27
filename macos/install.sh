56b09cff09b0dec843d3940f2652437334b96f08
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2022-02-27 13:06:59 +0530

    feat: use diff-so-fancy as the git diff tool

--- macos/install.sh
+++ macos/install.sh
@@ -108,6 +108,25 @@ git config --global core.editor "nvim"
 git config --global core.filemode false
 git config --global status.showuntrackedfiles all
 git config --global pull.rebase false
+git config --global init.defaultBranch main
+
+# Configure `diff-so-fancy` with git.
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RF"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "11"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.func       "146 bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
 
 # Create SSH key pair.
 ssh-keygen -t rsa -C "$EMAIL"
