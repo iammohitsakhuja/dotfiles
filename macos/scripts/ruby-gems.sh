9dfd65339390842486d57cf9364813d666432546
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2022-02-27 13:10:06 +0530

    feat: generate manpages for ruby gems

--- macos/scripts/ruby-gems.sh
+++ macos/scripts/ruby-gems.sh
@@ -8,6 +8,7 @@ export PATH="$BREW_PREFIX/opt/ruby/bin:$BREW_PREFIX/lib/ruby/gems/3.1.0/bin:$PAT
 # Install Ruby gems.
 echo "Installing Gems..."
 gem install colorls lolcat mdl
+gem manpages --update-all
 echo -e "Gems installed successfully\n"
 
 # TODO: Do something about adding manpages for gems to the path.
