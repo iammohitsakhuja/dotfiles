913baaa15c17d74bae75b8bad574297491c34120
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2025-08-28 01:47:13 +0530

    perf: perform only a shallow clone instead of a full clone for omz plugins

--- macos/scripts/shell-packages.sh
+++ macos/scripts/shell-packages.sh
@@ -34,8 +34,8 @@ echo -e "Oh-my-zsh installation successful!\n"
 
 # Install oh-my-zsh plugins.
 echo "Installing Oh-my-zsh plugins..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/fdw/yazi-zoxide-zsh.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/yazi-zoxide
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone --depth=1 https://github.com/fdw/yazi-zoxide-zsh.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/yazi-zoxide
 echo -e "Oh-my-zsh plugins installed successfully!\n"
