f01468a64f1f5d89808124c3b60aec355073b0cf
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2025-09-07 20:59:02 +0530

    perf: disable unused built-in plugins to improve performance

--- macos/home/.config/nvim/lua/config/lazy.lua
+++ macos/home/.config/nvim/lua/config/lazy.lua
@@ -51,7 +51,14 @@ require("lazy").setup({
   performance = {
     rtp = {
       disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
         "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
       },
     },
   },
