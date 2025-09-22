4b0221b8ad5191d6d044413cf877fea35736eae8
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2025-09-22 10:58:57 +0530

    chore(neovim): update lua lsp settings for better completion; add comments

--- macos/home/.config/nvim/after/lsp/lua_ls.lua
+++ macos/home/.config/nvim/after/lsp/lua_ls.lua
@@ -2,9 +2,20 @@ return {
     settings = {
         Lua = {
             runtime = { version = "LuaJIT" },
-            diagnostics = { globals = { "vim", "require" } },
-            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-            telemetry = { enable = false },
+            diagnostics = {
+                globals = { "vim", "require" },
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file("", true), -- Load entire workspace
+            },
+            hint = {
+                enable = true, -- Enable inlay hints.
+            },
+            completion = {
+                enable = true,
+                callSnippet = "Both",
+            },
         },
     },
 }
