32311ac2d2c0298004e6cf3fc7445e94a7f53697
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2025-10-10 10:20:42 +0530

    chore(jdtls): add JDK 25 as a runtime for `jdtls`

--- macos/home/.config/nvim/after/lsp/jdtls.lua
+++ macos/home/.config/nvim/after/lsp/jdtls.lua
@@ -4,6 +4,14 @@ return {
         java = {
             signatureHelp = { enabled = true },
             completion = { enabled = true },
+            configuration = {
+                runtimes = {
+                    {
+                        name = "JavaSE-25",
+                        path = "/Library/Java/JavaVirtualMachines/temurin-25.jdk/Contents/Home",
+                    },
+                },
+            },
             sources = {
                 organizeImports = { starThreshold = 9999, staticStarThreshold = 9999 },
             },
