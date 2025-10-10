07e1ba0d1dec6183d3cc49ff9812316df6522f56
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2025-10-10 10:26:55 +0530

    fix(neovim)!: fix double virtual column issue in neovim; make it dynamic as per project

--- macos/home/.config/nvim/lua/plugins/ui.lua
+++ macos/home/.config/nvim/lua/plugins/ui.lua
@@ -298,7 +298,7 @@ return {
         "lukas-reineke/virt-column.nvim",
         event = { "BufReadPost", "BufNewFile" },
         opts = {
-            virtcolumn = "+1,120", -- Highlight the 1st column after 120 columns.
+            virtcolumn = "+1", -- Highlight the 1st column after the max line length provided in `.editorconfig`
         },
     },
 
