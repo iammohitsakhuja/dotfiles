32f36c89a8d8cd2a2261b5a7aa0f2e3ab0ffb3a2
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2025-10-18 20:51:06 +0530

    fix(neovim): don't show fold icons in dap/dapui related windows

--- macos/home/.config/nvim/lua/plugins/statuscol.lua
+++ macos/home/.config/nvim/lua/plugins/statuscol.lua
@@ -6,10 +6,21 @@ return {
         local builtin = require("statuscol.builtin")
         require("statuscol").setup({
             relculright = true,
+            ft_ignore = {
+                "dapui_scopes",
+                "dapui_breakpoints",
+                "dapui_stacks",
+                "dapui_watches",
+                "dap-repl",
+                "dapui_console",
+            },
             segments = {
+                -- Fold icons without numbers
                 { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-                { text = { "%s" }, click = "v:lua.ScSa" },
-                { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+                -- Separator + Signs
+                { text = { " ", "%s" }, click = "v:lua.ScSa" },
+                -- Line numbers + separator
+                { text = { builtin.lnumfunc, " " }, condition = { true, builtin.not_empty }, click = "v:lua.ScLa" },
             },
         })
     end,
