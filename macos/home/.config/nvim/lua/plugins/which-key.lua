30f87fd22ffddf93d01909496087cb7c59bfe855
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2025-10-18 20:49:53 +0530

    feat(neovim)!: add helpful specs for `which-key` keybinds; change preset to `modern`

--- macos/home/.config/nvim/lua/plugins/which-key.lua
+++ macos/home/.config/nvim/lua/plugins/which-key.lua
@@ -5,14 +5,27 @@ return {
     -- Lazy loading the plugin does not work: https://github.com/folke/which-key.nvim/issues/981
     -- So we just defer it with VeryLazy.
     event = "VeryLazy",
+    ---@module "which-key.config"
+    ---@type wk.Opts
     opts = {
-        preset = "classic",
+        preset = "modern",
         plugins = {
             marks = false,
             registers = false,
         },
         win = {
             border = "rounded", -- We need this even if we set `winborder` globally via `vim.opt`
+        },
+        spec = {
+            { "<leader>b", group = "Buffer" },
+            { "<leader>c", group = "Conform or Trouble" },
+            { "<leader>d", group = "Debug" },
+            { "<leader>du", group = "Debug UI" },
+            { "<leader>f", group = "Telescope" },
+            { "<leader>h", group = "Git" },
+            { "<leader>s", group = "Session or Treesitter Swap" },
+            { "<leader>t", group = "Toggle" },
+            { "<leader>x", group = "Trouble" },
         },
     },
     keys = {
