55905be2f56bc284551c018939a502e824109d14
Manuinder Sekhon <sekhon.manuinder@gmail.com>
2025-11-10 22:33:52 +0530

    refactor(nvim): move the buffer delete keybindings to the plugin file

---
+++ macos/home/.config/nvim/lua/plugins/mini-bufremove.lua
@@ -0,0 +1,26 @@
+---@module "lazy"
+---@type LazySpec
+return {
+    "nvim-mini/mini.bufremove",
+    name = "mini.bufremove",
+    version = false,
+    keys = {
+        {
+            "<leader>bd",
+            function()
+                require("mini.bufremove").delete(0)
+            end,
+            mode = "n",
+            desc = "Delete buffer",
+        },
+        {
+            "<leader>bD",
+            function()
+                require("mini.bufremove").delete(0, true)
+            end,
+            mode = "n",
+            desc = "Delete buffer (force)",
+        },
+    },
+    opts = {},
+}
